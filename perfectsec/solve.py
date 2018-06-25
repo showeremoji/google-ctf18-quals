e30c7474668de2c37812b1df57799dc2cc0abfbe
Måns Magnusson <exoji2e@gmail.com>
2018-06-25 14:02:28 +0200

    [perfect secrecy] add solution

---
+++ perfectsec/solve.py
@@ -0,0 +1,99 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import sys, re
+sys.setrecursionlimit(10000)
+from pwn import *
+
+# returns g = gcd(a, b), x0, y0, 
+# where g = x0*a + y0*b
+def xgcd(a, b):
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while b != 0:
+        q, a, b = (a // b, b, a % b)
+        x0, x1 = (x1, x0 - q * x1)
+        y0, y1 = (y1, y0 - q * y1)
+    return (a, x0, y0)
+
+
+def get_rsadata():
+    pubkey = serialization.load_pem_public_key(
+            open('key_pub.pem', 'rb').read(), backend=default_backend())
+    N = pubkey.public_numbers().n
+    e = pubkey.public_numbers().e
+    KEY_BYTES = pubkey.key_size // 8
+    _, _, inv2 = xgcd(N, 2)
+    INV = pow(inv2, e, N)
+    return N, KEY_BYTES, INV
+
+
+def get_cache():
+    stored = {}
+    try:
+        with open('cache', 'r') as f:
+            data = f.read().strip().split('\n')
+            for l in data:
+                n, fn = map(int, l.split())
+                stored[n] = fn
+    except: pass
+    return stored
+
+
+def enc(v):
+    hx = hex(v).replace('L', '')[2:]
+    hx = hx if not len(hx)%2 else '0' + hx
+    raw = hx.decode('hex')
+    return '\x00'*(KEY_BYTES - len(raw)) + raw
+
+
+# gets the last bit of v**d%N from cache or from server.
+def getb(v):
+    if v in stored:
+        return stored[v]
+    back = [0, 0]
+    r = remote('perfect-secrecy.ctfcompetition.com', '1337')
+    payload = '\x00\x01' + enc(v)
+    r.send(payload)
+    data = r.recvn(100)
+    r.close()
+    for c in data:
+        back[ord(c)] += 1
+    
+    ret = 0 if back[0] > back[1] else 1
+    stored[v] = ret
+    with open('cache', 'a') as f:
+        f.write('{} {}\n'.format(v, ret))
+    return ret
+
+
+# returns last k bits of the message m, where c = m**e.
+# inv is a number such that 2**e*inv%N == 1
+def fl(c, k, N, inv):
+    if k == 1:
+        b = getb(c) 
+        return b
+    a = fl((c*inv)%N, k-1, N, inv)
+    b = getb(c) 
+    if b == 0:
+        return 2*a
+    else:
+        lo = (1<<(k+1)) - 1
+        return (2*a - N&lo)%(lo+1)
+
+
+def int2ascii(v):
+    hx = hex(v)[2:].replace('L', '')
+    hx = '0' + hx if len(hx)%2 else hx
+    return hx.decode('hex')
+
+
+if __name__ == '__main__':
+    with open('flag.txt', 'rb') as f:
+        asciicipher = f.read()
+        cipher = int(asciicipher.encode('hex'), 16)
+    N, KEY_BYTES, INV = get_rsadata()
+    stored = get_cache()
+    flag = fl(cipher, 1024, N, INV)
+    # print flag
+    padded = int2ascii(flag)
+    m = re.search('CTF\{.+\}', padded)
+    print(m.group(0))
